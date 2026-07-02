ee9bbc71e1e8200788e860a38628e88a36ff7fa0
Parth <parthrathi7278@gmail.com>
2026-07-02 13:53:33 +0530

    chore(misc): migrate and extend travel platform sources (27)

---
+++ backend/src/main/java/com/tripmind/api/repositories/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.tripmind.api.repositories;
+
+import com.tripmind.api.entities.Role;
+import com.tripmind.api.entities.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName name);
+}
